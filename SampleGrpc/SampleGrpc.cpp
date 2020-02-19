b121ee866b83056dfb46816394f915fbdbf83229
TimGe <gexiang@agvsz.com>
2020-02-19 11:04:25 +0800

    grpc 测试

---
+++ SampleGrpc/SampleGrpc.cpp
@@ -0,0 +1,16 @@
+// SampleGrpc.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+#include "test_function.h"
+
+int main()
+{
+	RunServer();
+	RunClient();
+
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
